2f7b00455323ed47107c2509a03e8fd71ec54fab
vanessa <v.aglagoh@gmail.com>
2022-09-15 09:55:42 +0100

    update adoration

--- adoration.js
+++ adoration.js
@@ -1,6 +1,6 @@
 function showHowMuchILoveYou (amount) {
     if (typeof amount == 'number' && Number.isInteger(amount) && amount > 0) {
-        return `I love you th${'i'.repeat(amount)}s much!`;
+        return `I love you th${'l'.repeat(amount)}s much!`;
 
     } else {
         throw new Error("I'm really sorry; you're just not my type.")
